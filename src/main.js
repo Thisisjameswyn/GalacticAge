4694da84fe55e9806de759c050fc6aad914f828d
James Wyn <thisisjameswyn@gmail.com>
2021-02-05 14:43:13 -0800

    add additional HTMl and jS, start fleshing out the UI

--- src/main.js
+++ src/main.js
@@ -7,14 +7,15 @@ import GalAge from './../src/ageCalc.js';
 
 
 $(document).ready(function() {
-  let newAge = new GalAge(50);
-  let mAge = newAge.getMerc();
-  let vAge = newAge.getVenus();
-  let marAge = newAge.getMars();
-  let jAge = newAge.getJupiter();
-  
-  console.log(mAge[0],mAge[1]);
-  console.log(vAge[0],vAge[1]);
-  console.log(marAge[0],marAge[1]);
-  console.log(jAge[0],jAge[1]);
+  $("#solarForm").submit(function(event) {
+    event.preventDefault();
+    const inputAge = $("#ageInput").val();
+    let newAge = new GalAge(inputAge);
+    let mAge = newAge.getMerc();
+    let vAge = newAge.getVenus();
+    let marAge = newAge.getMars();
+    let jAge = newAge.getJupiter();
+
+    
+  });
 });
